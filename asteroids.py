e42697a6bdb86f1e5dba41b3623660e3066a3606
konrad-py <konradd.py@gmail.com>
2025-09-14 11:56:06 +0200

    adding asteroids

---
+++ asteroids.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 14 11:20:39 2025
+
+@author: kg
+"""
+
+import pygame
+from circleshape import CircleShape
+
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+        
+    def draw(self, screen):
+        pygame.draw.circle(screen, 'white', self.position, self.radius, width=2)
+        
+    def update(self, dt):
+        self.position += self.velocity*dt
--- main.py
+++ main.py
@@ -3,28 +3,47 @@
 import pygame
 from constants import *
 from player import *
+from asteroidfield import *
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    asteroid_field = AsteroidField()
+
+    Player.containers = (updatable, drawable)
+    
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    
+    dt = 0
+    
+    
     
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
+    
+        updatable.update(dt)
+        
         screen.fill("black")
-        player.draw(screen)
+        
+        for obj in drawable:
+            obj.draw(screen)
+        
         pygame.display.flip()
         
+        # limit the framerate to 60 FPS
         dt = clock.tick(60)/1000
-    print("Starting Asteroids!")
-    print(f'Screen width: {SCREEN_WIDTH}')
-    print(f'Screen height: {SCREEN_HEIGHT}')
 
 
 if __name__ == "__main__":
