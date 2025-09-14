b34a588044a0b2f1b6927fe9873bd33d45e11d68
konrad-py <konradd.py@gmail.com>
2025-09-14 21:40:20 +0200

    creating shoots

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import pygame
 from constants import *
 from player import *
 from asteroidfield import *
-
+import sys
+from shot import *
 
 def main():
     pygame.init()
@@ -14,9 +15,10 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
-
+    shots = pygame.sprite.Group()
     
     Asteroid.containers = (asteroids, updatable, drawable)
+    Shot.containers = (shots, updatable, drawable)
     AsteroidField.containers = (updatable)
     asteroid_field = AsteroidField()
 
@@ -34,6 +36,16 @@ def main():
                 return
     
         updatable.update(dt)
+        
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game over!")
+                sys.exit()
+        for asteroid in asteroids:
+            for bullet in shots:
+                if bullet.collides_with(asteroid):
+                    asteroid.kill()
+                    bullet.kill()
         
         screen.fill("black")
         
---
+++ shot.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 14 20:42:11 2025
+
+@author: kg
+"""
+
+import pygame
+from circleshape import *
+from constants import *
+
+class Shot(CircleShape):
+    def __init__(self, x, y):
+        super().__init__(x, y, SHOT_RADIUS)
+        
+    def draw(self, screen):
+        pygame.draw.circle(screen, 'white', self.position, self.radius, width=2)
+        
+    def update(self, dt):
+        self.position += self.velocity*dt
